db4e23b97ff06037efa7f129574bf48c2b615d73
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-09-29 14:36:18 +0100

    INCK-20 Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* nav bar same for all (some components change depending on logged in) */}
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+            {/* home, not logged in */}
+          </Route>
+          <Route path="/profile">
+            <h1>Profile</h1>
+          </Route>
+          <Route path="/welcome">
+            {/* not sure about this path,  */}
+            <h1>Welcome</h1>
+          </Route>
+          <Route path="/stats">
+            {/* potentially could be integrated */}
+            <h1>Stats</h1>
+          </Route>
+          <Route path="/balance">
+            <h1>Balance</h1>
+          </Route>
+          <Route exact>
+            <h1>Oops! Page not found.</h1>
+            <Link to="/">Back to Home</Link>
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
