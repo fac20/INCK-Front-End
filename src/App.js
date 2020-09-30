41a6543a463cba41b2119c2ede128d57128071ca
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-09-30 11:13:35 +0100

    Removes home comps from imports Co-authored-by: Glenroy-Terence (Terry) <glenroyterence@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Switch, Route, Link, } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { NavBar } from "./components/nav.js";
-<<<<<<< HEAD
 import { Balance } from "./components/balance";
 import { Home } from "./components/home";
-||||||| d936bdf
-import Home from "./components/home.js";
-=======
-import { Balance } from "./components/balance";
->>>>>>> inck-3
 
 function App() {
   return (
