c2da942a23aa3cca786d4a5ce383524e9f87e739
ephie oyedoh <ephieo@rocketmail.com>
2020-09-30 21:29:28 +0100

    working on arranging the landing page with style components

---
+++ src/styled-components/Testimonial.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 30rem;
+  height: 10rem;
+  border: #0f0645 solid 3px;
+  box-shadow: 5px 10px #0f0645;
+  margin: 3rem;
+`;
