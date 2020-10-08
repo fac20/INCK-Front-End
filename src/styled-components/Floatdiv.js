5606a891802e321a04e1053db1c8035e59e6faa6
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2020-10-08 13:18:32 +0100

    working on css and spacing

--- src/styled-components/Floatdiv.js
+++ src/styled-components/Floatdiv.js
@@ -4,18 +4,22 @@ export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute;
+  height: 100vh;
   left: 0;
   top: 0;
   bottom: 0;
-  width: 40rem;
+  width: 50rem;
   background: #000426;
-  height: 95vh;
   top: 12rem;
   left: 0;
   margin: 0rem 0rem 0rem 0rem;
-  @media all and (max-width: 1212px) {
+  @media all and (max-width: 1000px) {
     position: static;
-    width: 500px;
+    width: 362px;
   }
+  @media all and (max-width: 424px) {
+    position: static;
+    width: 100vw;
+  }
+
 `;
